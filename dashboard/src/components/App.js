9db8046e86d47fdc5ae877e127d2c2ee37d39e71
Simon Liu <sliu.3110@gmail.com>
2021-01-27 10:32:22 -0800

    start writing nba api endpoints

--- dashboard/src/components/App.js
+++ dashboard/src/components/App.js
@@ -1,11 +1,45 @@
 import React from 'react';
+import nba from '../api/nba';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
+
+  onSearchSubmit = async (text) => {
+    const spaceIndex = text.search(' ');
+    const firstName = text.substring(0, spaceIndex);
+    const lastName = text.substring(spaceIndex+1);
+
+    const res = await nba.get(`/players?search=${lastName}&per_page=100`);
+    console.log(res.data.data);
+
+    const findPlayer = (arr, f) => {
+      // arr is an array of player objects
+      for (let i = 0; i < arr.length; i++){
+        if (arr[i].first_name.toLowerCase() === f.toLowerCase()){
+          return arr[i]
+        }
+      }
+    }
+
+    const player = await findPlayer(res, firstName);
+
+    const {id} = player;
+
+    // res.data.data.filter((p) => {
+    //   console.log(p.first_name);
+    //   if (p.first_name.toLowerCase() === firstName.toLowerCase()){
+    //     console.log(`found it sir: ${p}`)
+    //     return p;
+    //   } 
+    // })[0];
+    // should be saving the player id so it's easy to access stats
+    console.log(id);
+  }
+
   render() {
     return (
-      <div className="ui centered card">
-        <SearchBar />
+      <div className="ui centered container" style={{marginTop: '10px'}}>
+        <SearchBar onSearchSubmit={this.onSearchSubmit}/>
       </div>
     )
   }
--- dashboard/src/components/SearchBar.js
+++ dashboard/src/components/SearchBar.js
@@ -1,11 +1,26 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  state = {text: ''};
+
+  onFormChange = e => {
+    this.setState({text : e.target.value});
+  }
+
+  onFormSubmit = e => {
+    e.preventDefault();
+    this.props.onSearchSubmit(this.state.text);
+  }
+
   render() {
     return (
     <div className="ui search">
-      <form className="ui form">
-      <input class="prompt" type="text" placeholder="LeBron James"/>
+      <form onSubmit={this.onFormSubmit} className="ui form ">
+        <label htmlFor="search-bar">Search for an active player</label>
+      <input id="search-bar" className="prompt" type="text" placeholder="LeBron James"
+        onChange={this.onFormChange}
+        value={this.state.text}
+      />
       </form>
     </div>
     )
